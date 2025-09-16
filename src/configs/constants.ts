a72ec328ece32a8333ce8fe0446498f868fc06a1
dicodetomy-paradox <neha@eka.care>
2025-09-16 23:26:37 +0530

    url

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,5 @@
 const PRODUCTION_CONFIG = {
-    WEBSOCKET_URL: "wss://matrix.eka.care",
+    WEBSOCKET_URL: "wss://matrix-ws.eka.care",
     BASE_API_URL: "https://matrix.eka.care",
     X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzc3MDg4MTY2OTk2NzI0",
 };
