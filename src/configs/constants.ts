0a7ab70b9b9aac7e250c77edd937c35b88d60755
Geethanjali S <geethanjalis5657@gmail.com>
2025-09-30 19:34:48 +0530

    changed x agent id

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,19 +1,19 @@
 const PRODUCTION_CONFIG = {
     WEBSOCKET_URL: "wss://matrix-ws.eka.care",
     BASE_API_URL: "https://matrix.eka.care",
-    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzc3MDg4MTY2OTk2NzI0",
+    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzcxNzU5MTc2ODQzNTgzOTA=",
 };
 
 const DEVELOPMENT_CONFIG = { // can be used for local development
     WEBSOCKET_URL: "wss://matrix-ws.dev.eka.care",
     BASE_API_URL: "https://matrix.dev.eka.care",
-    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzc3MDg4MTY2OTk2NzI0",
+    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzcxNzU5MTc2ODQzNTgzOTA=",
 };
 
 const STAGING_CONFIG = {
     WEBSOCKET_URL: "wss://matrix-ws.dev.eka.care",
     BASE_API_URL: "https://matrix.dev.eka.care",
-    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzc3MDg4MTY2OTk2NzI0",
+    X_AGENT_ID: "MWZlZDRkYzktMTBmMS00OTFkLWEzNDMtZGM3MzIzZDM5N2VmIzcxNzU5MTc2ODQzNTgzOTA=",
 };
 
 // Use build mode instead of environment variable to avoid local .env dependency
